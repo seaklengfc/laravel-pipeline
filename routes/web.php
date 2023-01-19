e1df3eee351ee3e7d2cb41ca7fa10e90b3819549
seakleng <sayseaklengfc@gmail.com>
2023-01-20 00:51:52 +0700

    add connection name

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/connection', function () {
+    if(DB::connection()->getDatabaseName())
+    {
+        return "Connected to database ".DB::connection()->getDatabaseName();
+    }
+    return 'Connection Failed';
+});
